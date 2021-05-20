8de44cc261c6ea4eb043a796947913958ea11c4c
Agata Software <agatasoftware@gmail.com>
2021-05-20 15:35:15 +0100

    sample commit

---
+++ Learn.java
@@ -0,0 +1,28 @@
+    package test;
+
+public class Assignment1_MathematicalAndConcatenateOperators {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Assignment 1: Add Two numbers Multiply Two numbers Subtract Two numbers
+		 * Divide Two numbers Find the remainder of two numbers
+		 */
+
+		int num1 = 5;
+		int num2 = 10;
+		int sum = num1 + num2;
+		int multi = num1 * num2;
+		int subtract = num2 - num1;
+		int divide = num2 / num1;
+		int remainder = num2 % num1;
+
+		System.out.println("The sum of num1 and num2 is " + sum);
+		System.out.println("The result of mutiplication of num1 and num2 is " + multi);
+		System.out.println("The result of substraction of num2 and num1 is " + subtract);
+		System.out.println("The result of division of num2 and num1 is " + divide);
+		System.out.println("The remainder of num2 and num1 is " + remainder);
+
+		}
+
+}
